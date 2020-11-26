42e1f263cacc55164b3c241fafd0f8eccb577951
Ekrem Moralioglu <ekoremdev@gmail.com>
2020-11-26 14:36:17 +0400

    live native ui is done

--- android/app/src/main/java/com/connectionsoft/liveapp/CastActivity.java
+++ android/app/src/main/java/com/connectionsoft/liveapp/CastActivity.java
@@ -1,16 +1,26 @@
 package com.connectionsoft.liveapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.remotemonster.sdk.RemonCast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.webrtc.SurfaceViewRenderer;
+
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 
 public class CastActivity extends AppCompatActivity {
 
@@ -28,6 +38,18 @@ public class CastActivity extends AppCompatActivity {
     SurfaceViewRenderer castSurfaceView;
     Button castButton;
     Button stopButton;
+    ImageView imageView;
+
+    TextView twLiveShopping;
+    TextView twStreaming;
+    TextView twStartText;
+    TextView twStopText;
+
+    TextView txCloseScreen;
+    TextView txOnAir;
+    TextView txTimer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +61,28 @@ public class CastActivity extends AppCompatActivity {
         castButton = findViewById(R.id.castButton);
         stopButton = findViewById(R.id.stopButton);
         castSurfaceView = findViewById(R.id.local_video_view);
+        imageView = findViewById(R.id.imageView);
+
+        twLiveShopping = findViewById(R.id.liveShopping);
+        twStreaming = findViewById(R.id.streamingText);
+        twStartText = findViewById(R.id.startText);
+        twStopText = findViewById(R.id.stopText);
+
+        txCloseScreen = findViewById(R.id.closeScreen);
+        txOnAir = findViewById(R.id.onAir);
+        txTimer = findViewById(R.id.timer);
+
+        // set
+        castSurfaceView.setVisibility(View.GONE);
+        imageView.setVisibility(View.VISIBLE);
+        stopButton.setVisibility(View.GONE);
+        twLiveShopping.setVisibility(View.VISIBLE);
+        twStreaming.setVisibility(View.GONE);
+        twStartText.setVisibility(View.VISIBLE);
+        twStopText.setVisibility(View.GONE);
+        txOnAir.setVisibility(View.GONE);
+        txTimer.setVisibility(View.GONE);
+
 
         caster = RemonCast.builder()
                 .context(CastActivity.this)
@@ -52,6 +96,19 @@ public class CastActivity extends AppCompatActivity {
         castButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // make ui changes accordingly
+                castSurfaceView.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.GONE);
+                castButton.setVisibility(View.GONE);
+                stopButton.setVisibility(View.VISIBLE);
+                twLiveShopping.setVisibility(View.GONE);
+                twStreaming.setVisibility(View.VISIBLE);
+                twStartText.setVisibility(View.GONE);
+                twStopText.setVisibility(View.VISIBLE);
+                txOnAir.setVisibility(View.VISIBLE);
+                txTimer.setVisibility(View.VISIBLE);
+                //
+
                 caster.create(channelName);
             }
         });
@@ -59,9 +116,38 @@ public class CastActivity extends AppCompatActivity {
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // make ui changes accordingly
+                castSurfaceView.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+                castButton.setVisibility(View.VISIBLE);
+                stopButton.setVisibility(View.GONE);
+                twLiveShopping.setVisibility(View.VISIBLE);
+                twStreaming.setVisibility(View.GONE);
+                twStartText.setVisibility(View.VISIBLE);
+                twStopText.setVisibility(View.GONE);
+                txOnAir.setVisibility(View.GONE);
+                txTimer.setVisibility(View.GONE);
+                //
+                
                 caster.close();
             }
         });
 
+
+        txCloseScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(CastActivity.this);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.customview, viewGroup, false);
+                builder.setView(dialogView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+
     }
+
 }
